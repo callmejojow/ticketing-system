b3b27a05b389f992c45634667d93294e2b82636c
callmejojow <73418982+callmejojow@users.noreply.github.com>
2021-09-21 12:13:09 -0400

    Create Table

---
+++ database/migrations/2021_09_21_151435_create_tickets_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('manager_id')->nullable();
+            $table->unsignedInteger('location_id')->nullable();
+            $table->unsignedInteger('team_id')->nullable();
+
+            $table->string('status')->default('pending');
+            $table->string('priority')->default('nonurgent');
+            $table->string('img_url')->nullable();
+
+            $table->text('description');
+            $table->text('comment')->nullable();
+            $table->text('solution')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
