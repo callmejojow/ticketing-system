efa911f2f2cda50511e8ab0488a91226cd79951d
Hiro <suhiro@gmail.com>
2021-09-23 10:41:26 -0400

    base test

---
+++ resources/views/base.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<h1 class="text-2xl font-semibold text-green-500 ">content goes here</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('base');
 });
 
 
@@ -35,3 +35,6 @@ Route::get('tickets/create',[TicketController::class,'create'])->name('create');
 
 Route::delete('tickets/{id}',[UserController::class,'destroy'])->name('destroy');
 });
+
+
+
